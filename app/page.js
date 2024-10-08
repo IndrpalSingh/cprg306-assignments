3805f7c367437638d9bbe3d8dfdfdbe66c540b56
IndrpalSingh <167263199+IndrpalSingh@users.noreply.github.com>
2024-10-08 11:12:15 -0600

    some changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ export default function Page() {
 <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <a href="http://localhost:3000/week-2">week-2</a>
       <a href="http://localhost:3000/week-3">week-3</a>
+      <a href="http://localhost:3000/week-4">week-4</a>
       </main>
+
 
   )
 }    
